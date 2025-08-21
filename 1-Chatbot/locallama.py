35af90c10520d42a50ad60f40267dedbf2bc3e1b
BrijeshRakhasiya <brijeshrakhasiya587@gmail.com>
2025-08-22 00:11:30 +0530

    Chatbot app

---
+++ 1-Chatbot/locallama.py
@@ -0,0 +1,36 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers  import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
+os.environ['LANGCHAIN_TRACKING_V2'] = "true"
+
+
+# Prompt Template
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system" , "You are a helpful assistant , Please response to the user queries") , 
+        ("user","Question:{question}")
+    ]
+)
+
+
+
+# Streamlit Framework 
+
+st.title("Langchain Demo With OPENAI API")
+input_text = st.text_input("Search the topic u want")
+
+# Openai LLM
+llm = Ollama(model='llama3.2:latest')
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
